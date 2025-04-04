8c099c71e57bb12cd2f09c9a50d157e646596263
victor <victor.gavioli2004@gmail.com>
2025-04-04 19:06:44 +0200

    Maj for the app + guide for access the BDD

--- House/House/urls.py
+++ House/House/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import *
 from .views import *
-from profil.views import *
-from appareil.views import *
+from Profil.views import *
+from Appareil.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",default),
     path('profil/',ProfilView.as_view(), name="anything"),
     path('appareil/',AppareilView.as_view(), name="anything"),
 
--- House/House/views.py
+++ House/House/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def profil(request):
     return render(request,"page.html")
+
+def default(request):
+    return HttpResponse("<h1 align='center'>La base de donnée de Profil est dans <u>http://127.0.0.1:8000/profil/</u> et celle de Appareil dans <u>http://127.0.0.1:8000/appareil/</u>.<br>Le path admin n'a pas de superutilisateur enregistré.")
