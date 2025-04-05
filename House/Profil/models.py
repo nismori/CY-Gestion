907307e4f4475eeddfa121c41341b60e472ed5f1
neel958 <nael180904@gmail.com>
2025-04-05 16:16:30 +0200

    bdd update

--- House/Profil/models.py
+++ House/Profil/models.py
@@ -15,7 +15,7 @@ class Profil(models.Model):
     sexe = models.CharField(max_length=30,choices=SEXE_CHOICES,default="error")
     date_de_naissance = models.DateField(default="1900-01-01")
     type_de_membre = models.CharField(max_length=30,default="error")
-    #photo = models.ImageField(default="error")
+    #photo = models.ImageField(upload_to='images/', null = True, blank = True)
     prenom = models.CharField(max_length=30,default="error")
     nom = models.CharField(max_length=30,default="error")
     mot_de_passe = models.CharField(max_length=50,default="error")
--- src/components/background.jsx
+++ src/components/background.jsx
@@ -6,7 +6,7 @@ export function ShootingStarsAndStarsBackgroundDemo({ description = null }) {
   return (
     <div>
       <div className="fixed top-0 left-0 w-screen h-screen bg-neutral-900 -z-10">
-        <div className="py-5 absolute z-10 left-1/2 -translate-x-1/2 flex items-center gap-2 mx-auto font-georgia tracking-tight text-center text-transparent  mt-[20vh] md:text-xl md:leading-tight bg-clip-text bg-gradient-to-b from-neutral-800 via-white to-purple-900">
+        <div className="py-5 absolute z-10 left-1/2 -translate-x-1/2 flex items-center gap-2 mx-auto font-georgia tracking-tight text-center text-transparent  mt-[30vh] md:text-xl md:leading-tight bg-clip-text bg-gradient-to-b from-neutral-800 via-white to-purple-900">
           {description && <p>{description}</p>}
         </div>
 
