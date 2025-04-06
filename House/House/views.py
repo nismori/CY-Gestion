e6a404cbd34cce809895ace624742ba2f2c1e7e5
nismori <110066178+nismori@users.noreply.github.com>
2025-04-06 21:28:44 +0200

    default page

--- House/House/views.py
+++ House/House/views.py
@@ -5,4 +5,4 @@ def profil(request):
     return render(request,"page.html")
 
 def default(request):
-    return HttpResponse("<h1 align='center'>La base de donnée de Profil est dans <u>http://127.0.0.1:8000/profil/</u> et celle de Appareil dans <u>http://127.0.0.1:8000/appareil/</u>.<br>Le path admin n'a pas de superutilisateur enregistré.")
+    return render(request,"default.html")
